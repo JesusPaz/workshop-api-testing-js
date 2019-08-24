b9074d6bed3acde8a202f166d071f682e625b37e
Jesus Paz <jeherpabo@gmail.com>
2019-08-24 11:19:21 -0500

    test static code

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -2,76 +2,71 @@ const agent = require('superagent');
 const statusCode = require('http-status-codes');
 const chai = require('chai');
 
-const expect = chai.expect;
+const { expect } = chai;
 
 describe('First Api Tests', () => {
+  const urlAPI = 'https://httpbin.org';
 
-    const urlAPI = 'https://httpbin.org'
-
-    it('Consume GET Service', async () => {
-        const response = await agent.get(`${urlAPI}/ip`);
-        expect(response.status).to.equal(statusCode.OK);
-        expect(response.body).to.have.property('origin');
-    });
-
-
-    it('Consume GET Service with query parameters', async () => {
-        const requestBody = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
-
-        const response = await agent.get(`${urlAPI}/get`).query(requestBody);
-
-        expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.args).to.eql(requestBody);
-    });
+  it('Consume GET Service', async () => {
+    const response = await agent.get(`${urlAPI}/ip`);
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('origin');
+  });
 
 
-    it('HEAD request', async () => {
-        const response = await agent.head(`${urlAPI}/get`);
+  it('Consume GET Service with query parameters', async () => {
+    const requestBody = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
 
-        expect(response.status).to.equal(statusCode.OK);
-    });
+    const response = await agent.get(`${urlAPI}/get`).query(requestBody);
 
-
-    it('PUT request', async () => {
-
-        const requestBody = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
-
-        const response = await agent.put(`${urlAPI}/put`).query(requestBody);
-
-        expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.args).to.eql(requestBody);
-    });
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.args).to.eql(requestBody);
+  });
 
 
-    it('DELETE request', async () => {
-        const response = await agent.delete(`${urlAPI}/delete`);
+  it('HEAD request', async () => {
+    const response = await agent.head(`${urlAPI}/get`);
 
-        expect(response.status).to.equal(statusCode.OK);
-        expect(response.body).to.have.property('origin');
-    });
-
-
-    it('PATCH request', async () => {
-
-        const requestBody = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
-
-        const response = await agent.patch(`${urlAPI}/patch`).query(requestBody);
-
-        expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.args).to.eql(requestBody);
-    });
+    expect(response.status).to.equal(statusCode.OK);
+  });
 
 
+  it('PUT request', async () => {
+    const requestBody = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
+
+    const response = await agent.put(`${urlAPI}/put`).query(requestBody);
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.args).to.eql(requestBody);
+  });
+
+
+  it('DELETE request', async () => {
+    const response = await agent.delete(`${urlAPI}/delete`);
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('origin');
+  });
+
+
+  it('PATCH request', async () => {
+    const requestBody = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
+
+    const response = await agent.patch(`${urlAPI}/patch`).query(requestBody);
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.args).to.eql(requestBody);
+  });
 });
