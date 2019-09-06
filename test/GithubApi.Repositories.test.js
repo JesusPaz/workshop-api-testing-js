326ca44ebc505577e5f2c281c2618a2db4fd552a
Jesus Hernando Paz Bolaños <jeherpabo@gmail.com>
2019-09-06 06:25:02 -0500

    Get method (#7)
    * Test to api git hub, user and repos. Also, start the code to download the zip of the repo
    
    * download the zip and test that exists, also I removed the .env file and change the acces token to enviroment variable
    
    * change the url format
    
    * add assert not null and not undefined, also deleted comments
    
    * change to assert.exists()
    
    * add new changes and change the access token

---
+++ test/GithubApi.Repositories.test.js
@@ -0,0 +1,50 @@
+const agent = require('superagent');
+const statusCode = require('http-status-codes');
+const { expect } = require('chai');
+const fs = require('fs');
+const { assert } = require('chai');
+
+const urlBase = 'https://api.github.com';
+const githubUserName = 'aperdomob';
+
+describe('Github Api Test User', () => {
+  describe('Get method test', () => {
+    it('Validate user, company, location', async () => {
+      const response = await agent.get(`${urlBase}/users/${githubUserName}`)
+        .set('User-Agent', 'agent');
+      expect(response.status).to.equal(statusCode.OK);
+      expect(response.body.name).equal('Alejandro Perdomo');
+      expect(response.body.company).equal('PSL');
+      expect(response.body.location).equal('Colombia');
+    });
+
+    it('Find repos url and repo named asmine-awesome-report ', async () => {
+      const responseUsers = await agent.get(`${urlBase}/users/${githubUserName}`)
+        .set('User-Agent', 'agent');
+
+      const responseRepos = await agent.get(`${responseUsers.body.repos_url}`)
+        .set('User-Agent', 'agent');
+
+      const found = responseRepos.body.find((repo) => repo.name === 'jasmine-awesome-report');
+
+      assert.exists(found, 'Do not found any repository using the name "jasmine-awesome-report"');
+
+      expect(found.name).to.equal('jasmine-awesome-report');
+      expect(found.private).to.equal(false);
+      expect(found.description).to.equal('An awesome html report for Jasmine');
+    });
+
+    it('Download a zip file with the repo and check that exists', async () => {
+      const repoName = 'jasmine-awesome-report';
+      const href = `https://github.com/aperdomob/${repoName}/archive`;
+      const zipFile = 'master.zip';
+      const source = `${href}/${zipFile}`;
+
+      agent
+        .get(source)
+        .pipe(fs.createWriteStream(zipFile));
+
+      expect(fs.existsSync(zipFile)).to.equal(true);
+    });
+  });
+});
